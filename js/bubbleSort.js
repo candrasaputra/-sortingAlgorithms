809addd710c152dcd86ad93643b75f67238c4dd0
candra saputra <candrasaputra@live.com>
2019-07-24 14:48:29 +0700

    miltidimention

--- js/bubbleSort.js
+++ js/bubbleSort.js
@@ -43,5 +43,62 @@ function bubbleSortRecursive(array) {
     }
 }
 
-console.log(bubbleSort([6,8,1,4,3,2,1]));
-console.log(bubbleSortRecursive([6,8,1,4,3,2,1]));
+function bubbleSort2(array) {
+    let isNotSorted = true;
+    while (isNotSorted){
+        isNotSorted = false;
+
+        for (let i = 0; i < array.length; i++) {
+            let tmp;
+
+            if (array[i] > array[i+1]) {
+                tmp = array[i+1];
+                array[i+1] = array[i];
+                array[i] = tmp;
+
+                isNotSorted = true;
+            }
+        }
+    }
+
+    return array;
+}
+
+function bubbleSortMultidimention(array) {
+    let isNotSorted = true;
+    while (isNotSorted){
+        isNotSorted = false;
+
+        for (let i = 0; i < array.length - 1; i++) {
+            let tmp;
+
+            if (array[i][1] > array[i+1][1]) {
+                tmp = array[i+1];
+                array[i+1] = array[i];
+                array[i] = tmp;
+                
+                isNotSorted = true;
+            }
+
+            if (array[i][1] === array[i+1][1] && array[i][2] > array[i+1][2]) {
+                tmp = array[i+1];
+                array[i+1] = array[i];
+                array[i] = tmp;
+                
+                isNotSorted = true;
+            }
+        }
+    }
+    return array;
+}
+
+console.log(bubbleSortMultidimention(
+                        [
+                            [6,8,2],
+                            [6,8,1],
+                            [6,7,1]
+                        ])
+                    );
+
+// console.log(bubbleSort([6,8,1,4,3,2,1]));
+// console.log(bubbleSortRecursive([6,8,1,4,3,2,1]));
